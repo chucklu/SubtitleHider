5f50c3fde47efaf2bc6ae4f335ac88154c6a837a
Chuck LU <chucklu2015@outlook.com>
2022-06-19 23:58:02 +0800

    show hider

--- SubtitleHider/Hider.xaml.cs
+++ SubtitleHider/Hider.xaml.cs
@@ -10,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace SubtitleHider
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Hider.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Hider : Window
     {
-        public MainWindow()
+        public Hider()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Hider hider = new Hider();
-            hider.Topmost = true;
-            hider.Show();
         }
     }
 }
